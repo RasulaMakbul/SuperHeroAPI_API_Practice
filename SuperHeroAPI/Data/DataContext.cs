968d50c20905df28915e9917f82fc8d2a8fbe33e
RasulaMakbul <emucfs@gmail.com>
2022-03-16 12:48:09 +0600

    Add project files.

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=String.Empty;
+        public string FirstName { get; set; }=String.Empty;
+        public string LastName { get; set; }=String.Empty;
+        public string Place { get; set; } = String.Empty;
+    }
+}
